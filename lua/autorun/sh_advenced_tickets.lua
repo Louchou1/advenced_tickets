05fe3c34b92a492815f7e64a71474247e3f7fa72
Louchou1 <joseantonio29@sfr.fr>
2023-04-01 20:40:46 +0200

    sv

--- lua/autorun/sh_advenced_tickets.lua
+++ lua/autorun/sh_advenced_tickets.lua
@@ -1,5 +1,7 @@
 if SERVER then
     print("[Advenced Tickets] Loading...")
+    include("advenced_tickets\lua\autorun\sh_advenced_tickets.lua")
+    
     AddCSLuaFile("advenced_tickets\lua\client\cl_advenced_tickets.lua")
 
 else if CLIENT then
