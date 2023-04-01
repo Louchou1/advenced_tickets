066d562f4e8b90a135cc4d8f00b16257685f163e
Louchou1 <joseantonio29@sfr.fr>
2023-04-01 20:48:31 +0200

    add onplayerchat ticketcommand

--- lua/autorun/sh_advenced_tickets.lua
+++ lua/autorun/sh_advenced_tickets.lua
@@ -1,10 +1,10 @@
+MsgC( Color (200, 100, 100), "[Advenced Tickets]", Color (255, 255, 255), " - Chargements des fichiers...\n")
+
 if SERVER then
-    print("[Advenced Tickets] Loading...")
     include("advenced_tickets\lua\autorun\sh_advenced_tickets.lua")
     
     AddCSLuaFile("advenced_tickets\lua\client\cl_advenced_tickets.lua")
 
 else if CLIENT then
-    print("[Advenced Tickets] Loading...")
     include("advenced_tickets\lua\client\cl_advenced_tickets.lua")
 end
--- lua/client/cl_advenced_tickets.lua
+++ lua/client/cl_advenced_tickets.lua
@@ -1,1 +1,12 @@
 print("Hello world ! 2")
+
+hook.Add( "OnPlayerChat", "TicketCommand", function( ply, strText, bTeam, bDead ) 
+    if ( ply != LocalPlayer() ) then return end
+
+	strText = string.lower( strText )
+
+	if ( strText == "///" ) then
+		print( "Vous venez de faire un ticket à l'administration." )
+		return true
+	end
+end )
